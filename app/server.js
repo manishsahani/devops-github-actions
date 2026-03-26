0144814b619a20ae62ff4506efb584b06b7493b5
Manish Sahani <sahanimanish3@gmail.com>
2026-03-27 00:24:25 +0530

    Initialize Express server with health check endpoint

---
+++ app/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('DevOps Project Running 🚀');
+});
+
+app.get('/health', (req, res) => {
+  res.json({ status: "UP" });
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
