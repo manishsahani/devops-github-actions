18392d9615ef22ecfb85f0323f20a42c846f1df9
manish sahani <alwaysinattitude0@gmail.com>
2026-03-27 15:06:37 +0530

    adding all files

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('DevOps Project Running 🚀');
+  res.send('DevOps Project Running 🚀..');
 });
 
 app.get('/health', (req, res) => {
